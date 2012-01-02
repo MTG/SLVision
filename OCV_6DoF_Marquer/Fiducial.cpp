733c58b9fdd0b065bef567d5a50932bbfc3ea323
Daniel Gallardo <daniel.gallardo@upf.edu>
2012-01-02 17:18:37 +0100

    Licence header

--- OCV_6DoF_Marquer/Fiducial.cpp
+++ OCV_6DoF_Marquer/Fiducial.cpp
@@ -1,11 +1,31 @@
-//#include "StdAfx.h"
+/*
+	Daniel Gallardo Grassot
+	daniel.gallardo@upf.edu
+	Barcelona 2011
+
+	Licensed to the Apache Software Foundation (ASF) under one
+	or more contributor license agreements.  See the NOTICE file
+	distributed with this work for additional information
+	regarding copyright ownership.  The ASF licenses this file
+	to you under the Apache License, Version 2.0 (the
+	"License"); you may not use this file except in compliance
+	with the License.  You may obtain a copy of the License at
+
+		http://www.apache.org/licenses/LICENSE-2.0
+
+	Unless required by applicable law or agreed to in writing,
+	software distributed under the License is distributed on an
+	"AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+	KIND, either express or implied.  See the License for the
+	specific language governing permissions and limitations
+	under the License.
+*/
+
 #include "Fiducial.h"
 
 #define DISTANCE_OFFSET 30
 #define AREA_OFFSET 1000
 #define REMOVE_OFFSET 500
-
-//unsigned int Fiducial::id_generator = 0;
 
 Fiducial::Fiducial(void):x(0),y(0),area(0),a(cvPoint(0,0)),b(cvPoint(0,0)),c(cvPoint(0,0)),d(cvPoint(0,0)),fiducial_id(-1),orientation(0),is_updated(false),removed_time(-1)
 {
